a7f0822e76705a2fdb53b449b407034152f2a3a2
Ewaryst <59931691+xewerx@users.noreply.github.com>
2021-07-07 14:38:47 +0200

    Delete Projekt directory

--- Projekt/cookie.js
+++
@@ -1,5 +0,0 @@
-const back = document.querySelector(".buyButton");
-const logo = document.querySelector(".logo");
-
-back.addEventListener("click", () => window.location = 'index.html')
-logo.addEventListener("click", () => window.location = 'index.html')
